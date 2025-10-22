e4b4bd948456eea849a968a7ca30bfaa2925f930
Damian Birchler <damian.birchler@ost.ch>
2025-10-22 23:58:59 +0200

    Don't import "automagic" types into svelte components.

--- src/routes/simulations/(saved)/+page.svelte
+++ src/routes/simulations/(saved)/+page.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
-	import { type PageData } from './$types';
-
 	import { getBreadCrumbsStore } from './breadCrumbs';
 
-	export let data: PageData;
+	export let data;
 
 	const breadCrumbs = getBreadCrumbsStore();
 
