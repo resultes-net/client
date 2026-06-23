a5c4c15b111a6b4680ebdee569f59c91b03eb2c2
Damian Birchler <damian.birchler@ost.ch>
2026-06-23 17:18:31 +0200

    Prettify temperatures input mask.

--- src/routes/simulations/new/ptes/parameters/temperatures.svelte
+++ src/routes/simulations/new/ptes/parameters/temperatures.svelte
@@ -1,10 +1,6 @@
 <script lang="ts">
-	import { type PopupSettings, popup } from '@skeletonlabs/skeleton';
-
-	import { Info } from 'lucide-svelte';
-
 	import { t } from '$lib/i18n/translations';
-	import { parseAndClampInputValue, clampValue } from '$lib/utils';
+	import { clampValue, parseAndClampInputValue } from '$lib/utils';
 
 	import { type Temperatures } from '$lib/openapi/generated/model/temperatures';
 
@@ -14,12 +10,6 @@
 	const MAX_TEMPERATURE_DEGC = 200;
 
 	let mode: 'absolute' | 'relative' = 'relative';
-
-	const presetsInfoHoverPopupSettings: PopupSettings = {
-		event: 'hover',
-		target: 'presetsInfoHoverPopup',
-		placement: 'top'
-	};
 
 	function clampTemperature(value: number): number {
 		return clampValue(value, MIN_TEMPERATURE_DEGC, MAX_TEMPERATURE_DEGC);
@@ -105,162 +95,161 @@
 	}
 </script>
 
-<div data-popup="presetsInfoHoverPopup">
-	<div class="card p-4 variant-filled-secondary z-50">
-		<p>{$t('common.temperaturesArePresetFromDemandSetpoint')}</p>
-		<div class="arrow variant-filled-secondary" />
+<div class="m-2 p-2">
+	<div class="grid grid-cols-[--input-grid-cols] items-center gap-y-[--input-gap-y]">
+		<label for="setpoint-temperatures-input-mode">{$t('common.temperatureInputMode')}</label>
+		<select id="setpoint-temperatures-input-mode" class="select" bind:value={mode}>
+			<option value="absolute">{$t('common.setpointTemperaturesInputModeAbsolute')}</option>
+			<option value="relative">{$t('common.setpointTemperaturesInputModeRelative')}</option>
+		</select>
+	</div>
+
+	<hr class="mt-2 mb-3 mr-1" />
+
+	<div class="grid grid-cols-[--input-grid-cols] items-center gap-y-[--input-gap-y]">
+		<label for="demand-setpoint-temperature">{$t('common.demandSetpointTemperature')}</label>
+		<div class="input-group input-group-divider grid grid-cols-[--input-unit-grid-cols]">
+			<input
+				class="input"
+				id="demand-setpoint-temperature"
+				title={$t('common.demandSetpointTemperature')}
+				type="number"
+				value={temperatures.demand_setpoint_degC}
+				min={MIN_TEMPERATURE_DEGC}
+				max={MAX_TEMPERATURE_DEGC}
+				on:change={onDemandSetpointChanged}
+			/>
+			<div><span class="flex flex-grow justify-center">°C</span></div>
+		</div>
+
+		{#if mode === 'absolute'}
+			<label for="boiler-output-setpoint-temperature"
+				>{$t('common.boilerOutputSetpointTemperature')}</label
+			>
+			<div class="input-group input-group-divider grid grid-cols-[--input-unit-grid-cols]">
+				<input
+					class="input"
+					id="boiler-output-setpoint-temperature"
+					title={$t('common.boilerOutputSetpointTemperature')}
+					type="number"
+					value={temperatures.boiler_output_setpoint_degC}
+					min={MIN_TEMPERATURE_DEGC}
+					max={MAX_TEMPERATURE_DEGC}
+					on:change={(e) => onTemperatureChanged(e, 'boiler_output_setpoint_degC')}
+				/>
+				<div><span class="flex flex-grow justify-center">°C</span></div>
+			</div>
+
+			<label for="heat-pump-output-setpoint-temperature"
+				>{$t('common.heatPumpOutputSetpointTemperature')}</label
+			>
+			<div class="input-group input-group-divider grid grid-cols-[--input-unit-grid-cols]">
+				<input
+					class="input"
+					id="heat-pump-output-setpoint-temperature"
+					title={$t('common.heatPumpOutputSetpointTemperature')}
+					type="number"
+					value={temperatures.heat_pump_output_setpoint_degC}
+					min={MIN_TEMPERATURE_DEGC}
+					max={MAX_TEMPERATURE_DEGC}
+					on:change={(e) => onTemperatureChanged(e, 'heat_pump_output_setpoint_degC')}
+				/>
+				<div><span class="flex flex-grow justify-center">°C</span></div>
+			</div>
+
+			<label for="maximum-storage-temperature">{$t('common.maximumStorageTemperature')}</label>
+			<div class="input-group input-group-divider grid grid-cols-[--input-unit-grid-cols]">
+				<input
+					class="input"
+					id="maximum-storage-temperature"
+					title={$t('common.maximumStorageTemperature')}
+					type="number"
+					value={temperatures.storage_maximum_degC}
+					min={MIN_TEMPERATURE_DEGC}
+					max={MAX_TEMPERATURE_DEGC}
+					on:change={(e) => onTemperatureChanged(e, 'storage_maximum_degC')}
+				/>
+				<div><span class="flex flex-grow justify-center">°C</span></div>
+			</div>
+
+			<label for="collector-output-setpoint-temperature"
+				>{$t('common.collectorOutputSetpointTemperature')}</label
+			>
+			<div class="input-group input-group-divider grid grid-cols-[--input-unit-grid-cols]">
+				<input
+					class="input"
+					id="collector-output-setpoint-temperature"
+					title={$t('common.collectorOutputSetpointTemperature')}
+					type="number"
+					value={temperatures.collector_output_setpoint_degC}
+					min={MIN_TEMPERATURE_DEGC}
+					max={MAX_TEMPERATURE_DEGC}
+					on:change={(e) => onTemperatureChanged(e, 'collector_output_setpoint_degC')}
+				/>
+				<div><span class="flex flex-grow justify-center">°C</span></div>
+			</div>
+		{:else if mode === 'relative'}
+			<label for="boiler-output-setpoint-temperature">{$t('common.dtBoilerDemand')}</label>
+			<div class="input-group input-group-divider grid grid-cols-[--input-unit-grid-cols]">
+				<input
+					class="input"
+					id="boiler-output-setpoint-temperature"
+					title={$t('common.dtBoilerDemand')}
+					type="number"
+					value={getDelta('demand_setpoint_degC')}
+					on:change={(e) => onDeltaChanged(e, 'demand_setpoint_degC')}
+				/>
+				<div><span class="flex flex-grow justify-center">K</span></div>
+			</div>
+
+			<label for="heat-pump-output-setpoint-temperature">{$t('common.dtHeatPumpBoiler')}</label>
+			<div class="input-group input-group-divider grid grid-cols-[--input-unit-grid-cols]">
+				<input
+					class="input"
+					id="heat-pump-output-setpoint-temperature"
+					title={$t('common.dtHeatPumpBoiler')}
+					type="number"
+					value={getDelta('boiler_output_setpoint_degC')}
+					on:change={(e) => onDeltaChanged(e, 'boiler_output_setpoint_degC')}
+				/>
+				<div><span class="flex flex-grow justify-center">K</span></div>
+			</div>
+
+			<label for="maximum-storage-temperature">{$t('common.dtStorageMaxHeatPump')}</label>
+			<div class="input-group input-group-divider grid grid-cols-[--input-unit-grid-cols]">
+				<input
+					class="input"
+					id="maximum-storage-temperature"
+					title={$t('common.dtStorageMaxHeatPump')}
+					type="number"
+					value={getDelta('heat_pump_output_setpoint_degC')}
+					on:change={(e) => onDeltaChanged(e, 'heat_pump_output_setpoint_degC')}
+				/>
+				<div><span class="flex flex-grow justify-center">K</span></div>
+			</div>
+
+			<label for="collector-output-setpoint-temperature">{$t('common.dtCollectorStorageMax')}</label
+			>
+			<div class="input-group input-group-divider grid grid-cols-[--input-unit-grid-cols]">
+				<input
+					class="input"
+					id="collector-output-setpoint-temperature"
+					title={$t('common.dtCollectorStorageMax')}
+					type="number"
+					value={getDelta('storage_maximum_degC')}
+					on:change={(e) => onDeltaChanged(e, 'storage_maximum_degC')}
+				/>
+				<div><span class="flex flex-grow justify-center">K</span></div>
+			</div>
+		{:else}
+			<div class="text-red-500">Unknown mode: {mode}</div>
+		{/if}
 	</div>
 </div>
 
-<div class="flex flex-col gap-4 mb-4">
-	<label for="temperature-mode" class="font-bold">{$t('common.temperatureInputMode')}</label>
-	<select id="temperature-mode" class="select w-full" bind:value={mode}>
-		<option value="absolute">{$t('common.temperatureModeAbsolute')}</option>
-		<option value="relative">{$t('common.temperatureModeRelative')}</option>
-	</select>
-</div>
-
-<div class="grid grid-cols-[--input-grid-cols] items-center gap-y-[--input-gap-y] m-2 p-2">
-	<label for="demand-setpoint-temperature">{$t('common.demandSetpointTemperature')}</label>
-	<div class="input-group input-group-divider grid grid-cols-[1fr_auto_auto] items-center">
-		<input
-			class="input"
-			id="demand-setpoint-temperature"
-			title={$t('common.demandSetpointTemperature')}
-			type="number"
-			value={temperatures.demand_setpoint_degC}
-			min={MIN_TEMPERATURE_DEGC}
-			max={MAX_TEMPERATURE_DEGC}
-			on:change={onDemandSetpointChanged}
-		/>
-		<div><span class="flex flex-grow justify-center">°C</span></div>
-		<button
-			class="btn variant-filled-primary [&>*]:pointer-events-none"
-			use:popup={presetsInfoHoverPopupSettings}
-		>
-			<Info />
-		</button>
-	</div>
-
-	{#if mode === 'absolute'}
-		<label for="boiler-output-setpoint-temperature"
-			>{$t('common.boilerOutputSetpointTemperature')}</label
-		>
-		<div class="input-group input-group-divider grid grid-cols-[--input-unit-grid-cols]">
-			<input
-				class="input"
-				id="boiler-output-setpoint-temperature"
-				title={$t('common.boilerOutputSetpointTemperature')}
-				type="number"
-				value={temperatures.boiler_output_setpoint_degC}
-				min={MIN_TEMPERATURE_DEGC}
-				max={MAX_TEMPERATURE_DEGC}
-				on:change={(e) => onTemperatureChanged(e, 'boiler_output_setpoint_degC')}
-			/>
-			<div><span class="flex flex-grow justify-center">°C</span></div>
-		</div>
-
-		<label for="heat-pump-output-setpoint-temperature"
-			>{$t('common.heatPumpOutputSetpointTemperature')}</label
-		>
-		<div class="input-group input-group-divider grid grid-cols-[--input-unit-grid-cols]">
-			<input
-				class="input"
-				id="heat-pump-output-setpoint-temperature"
-				title={$t('common.heatPumpOutputSetpointTemperature')}
-				type="number"
-				value={temperatures.heat_pump_output_setpoint_degC}
-				min={MIN_TEMPERATURE_DEGC}
-				max={MAX_TEMPERATURE_DEGC}
-				on:change={(e) => onTemperatureChanged(e, 'heat_pump_output_setpoint_degC')}
-			/>
-			<div><span class="flex flex-grow justify-center">°C</span></div>
-		</div>
-
-		<label for="maximum-storage-temperature">{$t('common.maximumStorageTemperature')}</label>
-		<div class="input-group input-group-divider grid grid-cols-[--input-unit-grid-cols]">
-			<input
-				class="input"
-				id="maximum-storage-temperature"
-				title={$t('common.maximumStorageTemperature')}
-				type="number"
-				value={temperatures.storage_maximum_degC}
-				min={MIN_TEMPERATURE_DEGC}
-				max={MAX_TEMPERATURE_DEGC}
-				on:change={(e) => onTemperatureChanged(e, 'storage_maximum_degC')}
-			/>
-			<div><span class="flex flex-grow justify-center">°C</span></div>
-		</div>
-
-		<label for="collector-output-setpoint-temperature"
-			>{$t('common.collectorOutputSetpointTemperature')}</label
-		>
-		<div class="input-group input-group-divider grid grid-cols-[--input-unit-grid-cols]">
-			<input
-				class="input"
-				id="collector-output-setpoint-temperature"
-				title={$t('common.collectorOutputSetpointTemperature')}
-				type="number"
-				value={temperatures.collector_output_setpoint_degC}
-				min={MIN_TEMPERATURE_DEGC}
-				max={MAX_TEMPERATURE_DEGC}
-				on:change={(e) => onTemperatureChanged(e, 'collector_output_setpoint_degC')}
-			/>
-			<div><span class="flex flex-grow justify-center">°C</span></div>
-		</div>
-	{:else if mode === 'relative'}
-		<label for="boiler-output-setpoint-temperature">{$t('common.dtBoilerDemand')}</label>
-		<div class="input-group input-group-divider grid grid-cols-[--input-unit-grid-cols]">
-			<input
-				class="input"
-				id="boiler-output-setpoint-temperature"
-				title={$t('common.dtBoilerDemand')}
-				type="number"
-				value={getDelta('demand_setpoint_degC')}
-				on:change={(e) => onDeltaChanged(e, 'demand_setpoint_degC')}
-			/>
-			<div><span class="flex flex-grow justify-center">K</span></div>
-		</div>
-
-		<label for="heat-pump-output-setpoint-temperature">{$t('common.dtHeatPumpBoiler')}</label>
-		<div class="input-group input-group-divider grid grid-cols-[--input-unit-grid-cols]">
-			<input
-				class="input"
-				id="heat-pump-output-setpoint-temperature"
-				title={$t('common.dtHeatPumpBoiler')}
-				type="number"
-				value={getDelta('boiler_output_setpoint_degC')}
-				on:change={(e) => onDeltaChanged(e, 'boiler_output_setpoint_degC')}
-			/>
-			<div><span class="flex flex-grow justify-center">K</span></div>
-		</div>
-
-		<label for="maximum-storage-temperature">{$t('common.dtStorageMaxHeatPump')}</label>
-		<div class="input-group input-group-divider grid grid-cols-[--input-unit-grid-cols]">
-			<input
-				class="input"
-				id="maximum-storage-temperature"
-				title={$t('common.dtStorageMaxHeatPump')}
-				type="number"
-				value={getDelta('heat_pump_output_setpoint_degC')}
-				on:change={(e) => onDeltaChanged(e, 'heat_pump_output_setpoint_degC')}
-			/>
-			<div><span class="flex flex-grow justify-center">K</span></div>
-		</div>
-
-		<label for="collector-output-setpoint-temperature">{$t('common.dtCollectorStorageMax')}</label>
-		<div class="input-group input-group-divider grid grid-cols-[--input-unit-grid-cols]">
-			<input
-				class="input"
-				id="collector-output-setpoint-temperature"
-				title={$t('common.dtCollectorStorageMax')}
-				type="number"
-				value={getDelta('storage_maximum_degC')}
-				on:change={(e) => onDeltaChanged(e, 'storage_maximum_degC')}
-			/>
-			<div><span class="flex flex-grow justify-center">K</span></div>
-		</div>
-	{:else}
-		<div class="text-red-500">Unknown mode: {mode}</div>
-	{/if}
-</div>
+<style>
+	* {
+		--input-grid-cols: 66% 33%;
+		--input-unit-grid-cols: 66% 33%;
+	}
+</style>
