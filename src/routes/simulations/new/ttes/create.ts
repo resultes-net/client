13a19f53529528fa57aaff8fc6d4a9291177addb
Damian Birchler <damian.birchler@ost.ch>
2026-08-07 22:57:08 +0200

    Update `openapi-schema`.

--- src/routes/simulations/new/ttes/create.ts
+++ src/routes/simulations/new/ttes/create.ts
@@ -7,12 +7,12 @@ import { createDefaultWasteHeatRecoverySource } from "$lib/parameters/createDefa
 export function createDefaultParameters(): TtesParametersInput {
     const defaultParameters: TtesParametersInput = {
         type: "ttes",
-        time: { start: 0, stop: 3 * 365 * 24, dt_sim: 5 / 60 },
+        time: { start: 0, stop: 5 * 365 * 24, dt_sim: 5 / 60 },
         demand: createDefaultDemand(),
         collector_field: createDefaultCollectorField(),
         waste_heat_recovery_source: createDefaultWasteHeatRecoverySource(),
         storage: {
-            heat_conductance_kW_per_m2_per_K: 0.08e-3,
+            insulation_thickness_cm: 6.0,
             ports_relative_heights_1: {
                 top: 0.99,
                 middle: 0.50,
@@ -23,8 +23,6 @@ export function createDefaultParameters(): TtesParametersInput {
                 value: 1
             },
             height_to_diameter_ratio_1: 5,
-            location: "above-ground-free-standing",
-
         },
         control: createDefaultControl()
     };
