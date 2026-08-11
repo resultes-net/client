36a7c3a4c2fa55d5102174a300daa556888f4589
Damian Birchler <damian.birchler@ost.ch>
2026-08-11 15:21:32 +0200

    [TTES] Show plots.

--- src/lib/outputs/ttes/createDisplayResults.ts
+++ src/lib/outputs/ttes/createDisplayResults.ts
@@ -3,6 +3,13 @@ import type { CreateDisplayResults, DisplayResult } from '$lib/outputs/displayRe
 
 export const createDisplayResults: CreateDisplayResults = () => {
     const displayResults: DisplayResult[] = [
+        { id: 'energyBalance', title: 'Monthly energy balance of the system', path: '/balance/balance-monthly-A4.png', data: null },
+        { id: 'boiler', title: 'Boiler power', path: '/boiler/boiler-hourly-A4.png', data: null },
+        { id: 'sink', title: 'Demand power', path: '/sink/sink-hourly-A4.png', data: null },
+        { id: 'solarQt', title: 'Collector Q vs. T plot', path: '/solar/q_t-A4.png', data: null },
+        { id: 'solarPHourly', title: 'Collector power hourly', path: '/solar/solar-hourly-A4.png', data: null },
+        { id: 'solarPMonthly', title: 'Collector power monthly', path: '/solar/solar-monthly-A4.png', data: null },
+        { id: 'source', title: 'Additional source power', path: '/source/source-hourly-A4.png', data: null }
     ];
 
     return displayResults;
