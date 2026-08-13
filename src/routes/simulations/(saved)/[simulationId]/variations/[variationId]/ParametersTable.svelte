161169b6e123a417c25cf32f2550bc1fc6ddd924
Damian Birchler <damian.birchler@ost.ch>
2026-08-13 12:45:11 +0200

    Extract TES parameters table rows.

--- src/routes/simulations/(saved)/[simulationId]/variations/[variationId]/ParametersTable.svelte
+++ src/routes/simulations/(saved)/[simulationId]/variations/[variationId]/ParametersTable.svelte
@@ -3,6 +3,7 @@
 
 	import { t } from '$lib/i18n/translations';
 	import type { ParametersOutput } from 'src/lib/openapi/generated/model/parametersOutput';
+	import PtesTableRows from './parametersTable/tes/PtesTableRows.svelte';
 
 	export let parameters: ParametersOutput;
 
@@ -152,46 +153,7 @@
 						</thead>
 						<tbody>
 							{#if parameters.values.type === 'ptes'}
-								<tr>
-									<td>{$t('common.storageVolume')}</td>
-									<td>{parameters.values.storage.volume.value}</td>
-									{#if parameters.values.storage.volume.scaling == 'absolute_m3'}
-										<td>m<sup>3</sup></td>
-										<td />
-									{:else if parameters.values.storage.volume.scaling == 'relative_to_demand_m3_per_MWh'}
-										<td>m<sup>3</sup>MWh<sup>-1</sup></td>
-										<td
-											>{(parameters.values.storage.volume.value * yearlyHeatDemandMWh).toFixed(0)} m<sup
-												>3</sup
-											>
-										</td>
-									{:else if parameters.values.storage.volume.scaling == 'relative_to_collector_area_m3_per_m2'}
-										<td>m<sup>3</sup>m<sup>-2</sup></td>
-										<td
-											>{(parameters.values.storage.volume.value * collectorFieldAreaM2).toFixed(0)} m<sup
-												>3</sup
-											>
-										</td>
-									{/if}
-								</tr>
-								<tr>
-									<td>{$t('common.portHeightTop')}</td>
-									<td>{parameters.values.storage.ports_relative_heights_1.top}</td>
-									<td>&percnt;</td>
-									<td />
-								</tr>
-								<tr>
-									<td>{$t('common.portHeightMiddle')}</td>
-									<td>{parameters.values.storage.ports_relative_heights_1.middle}</td>
-									<td>&percnt;</td>
-									<td />
-								</tr>
-								<tr>
-									<td>{$t('common.portHeightBottom')}</td>
-									<td>{parameters.values.storage.ports_relative_heights_1.bottom}</td>
-									<td>&percnt;</td>
-									<td />
-								</tr>
+								<PtesTableRows {parameters} {yearlyHeatDemandMWh} {collectorFieldAreaM2} />
 							{/if}
 						</tbody>
 					</table>
