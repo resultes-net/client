3c01f18768eab4415b308d907598cb2e2d7e2fe2
Damian Birchler <damian.birchler@ost.ch>
2026-08-28 10:41:20 +0200

    [BTES] More sensible default parameter values.

--- src/routes/simulations/new/btes/create.ts
+++ src/routes/simulations/new/btes/create.ts
@@ -9,15 +9,15 @@ export function createDefaultParameters() {
         type: "btes",
         time: { start: 0, stop: 5 * 365 * 24, dt_sim: 5 / 60 },
         demand: createDefaultDemand(),
-        collector_field: createDefaultCollectorField({ area_relative_to_demand_m2_per_MWh: 0.15 }),
+        collector_field: createDefaultCollectorField({ area_relative_to_demand_m2_per_MWh: 2.0 }),
         waste_heat_recovery_source: createDefaultWasteHeatRecoverySource(),
         storage: {
             volume: {
                 scaling: "relative_to_collector_area_m3_per_m2",
-                value: 2.7
+                value: 3.0
             },
         },
-        control: createDefaultControl({ storage_temperature_maximum_degC: 98 })
+        control: createDefaultControl({ storage_temperature_maximum_degC: 75 })
     };
 
     return defaultParameters;
